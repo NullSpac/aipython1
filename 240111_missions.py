3d1194f8e063a505add49f4d417d1ac10025c476
NullSpac <131910082+NullSpac@users.noreply.github.com>
2024-01-11 13:34:16 +0900

    Add files via upload

---
+++ 240111_missions.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt 
+import pandas as pd
+import numpy as np 
+import seaborn as sns 
+
+##### mission1
+# x = np.arange(1, 11)
+# y = np.random.randint(1, 30, 10)
+# plt.scatter(x, y, c='r', marker='s')
+# plt.show()
+
+
+# ##### mission2
+# plt.rc('font', family='NanumGothic')
+# df = pd.read_excel('data1.xlsx')
+# # print(df)
+# plt.plot(df['name'], df['kor'], 'go--', label='국어점수')
+# plt.plot(df['name'], df['math'], 'r*-', label='수학점수')
+# plt.legend()
+# plt.title('성적 그래프')
+# plt.xlabel('이름')
+# plt.ylabel('점수')
+# plt.show()
+
+
+##### mission3
+##문제1
+# penguins = pd.read_csv('penguins.csv')
+# print(penguins)
+# print(penguins.columns)
+# # penguins.info()
+# sns.barplot(x='species', y='body_mass_g', hue='sex', data=penguins, errorbar='sd')
+# plt.show()
+##문제2
+# cc = penguins.loc[:, 'bill_length_mm':'body_mass_g']
+# sns.heatmap(data=penguins, annot=True, fmt='.2f', cbar=False)
+# plt.show()
